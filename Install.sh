ff22a98555a49b19f3edf7340185a21fed9b2468
Mr_HackPro <44978328+Systemvuln@users.noreply.github.com>
2019-08-09 17:05:00 +0700

    Sh

---
+++ Install.sh
@@ -0,0 +1,3 @@
+z="
+";Kz='pkg ';Oz='p';Yz='h';Uz='pt';Cz='echo';Dz=' "Tu';Nz='unzi';Xz='ro.s';Qz='crip';Bz='r';Rz='t.zi';Wz='ackp';Gz='enta';Fz=' seb';Mz='all ';Az='clea';Hz='r"';Jz='p 1';Iz='slee';Lz='inst';Sz='cd e';Vz='sh h';Ez='nggu';Tz='ncri';Pz='p en';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$z$Kz$Lz$Mz$Nz$Oz$z$Nz$Pz$Qz$Rz$Oz$z$Sz$Tz$Uz$z$Vz$Wz$Xz$Yz"
